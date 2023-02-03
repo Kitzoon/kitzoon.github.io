16cbaf366605d7b5466700e7b6151617cd794783
Kitzoon <68196838+Kitzoon@users.noreply.github.com>
2023-02-03 10:36:54 -0500

    Add files via upload

---
+++ title.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    if (document.title === "Rogue Hub - Nolix") {
+        document.title = "Rogue Hub - Kyron"
+    } else {
+        document.title = "Rogue Hub - Nolix"
+    }
+}, 3000)
